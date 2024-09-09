d4d5055296ad79b5fa6c884be152184e027e75d7
mukaritto <mukarit2006@gmail.com>
2024-09-10 03:27:50 +0500

    Add files via upload

---
+++ MathFunctions.java
@@ -0,0 +1,26 @@
+public class MathFunctions {
+
+    public void add(double a, double b) {
+        System.out.println(a+b);
+    }
+
+    public void sub(double a, double b) {
+        System.out.println(a-b);
+    }
+
+    public void multiply(double a, double b) {
+        System.out.println(a*b);
+    }
+
+    public void div(double a, double b) {
+        System.out.println(a/b);
+    }
+
+    public void abs(double a) {
+        System.out.println(Math.abs(a));
+    }
+
+    public void pow(double a) {
+        System.out.println(Math.pow(a, 2));
+    }
+}
